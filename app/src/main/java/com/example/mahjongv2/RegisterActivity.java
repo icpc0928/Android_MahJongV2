087110282cede72da0fc642ca47b644473b3298a
Leo <icpc0928@gmail.com>
2020-04-04 17:27:04 +0800

    register only

---
+++ app/src/main/java/com/example/mahjongv2/RegisterActivity.java
@@ -0,0 +1,117 @@
+package com.example.mahjongv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText name , email , password , c_password;
+    private Button btn_regist;
+    private ProgressBar loading;
+    private static String URL_REGIST="http://192.168.0.101/android_register_login/register.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        loading = findViewById(R.id.loading);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        c_password = findViewById(R.id.c_password);
+        btn_regist = findViewById(R.id.btn_regist);
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Regist();
+            }
+        });
+
+    }
+
+
+    //btn做的監聽
+    private void Regist(){
+        //先跑圈圈
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility(View.GONE);
+        //.trim() 移除前面所有空白字元
+        final String name = this.name.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            Log.v("leo",response);
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if(success.equals("1")){
+                                Log.v("leo","debug2");
+                                Toast.makeText(RegisterActivity.this,"Register Success!",Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        } catch (Exception e) {
+                            Log.v("leo","Exception:  "+e.toString());
+                            e.printStackTrace();
+                            Toast.makeText(RegisterActivity.this,"Register Error!  " + e.toString(),Toast.LENGTH_SHORT).show();
+                            loading.setVisibility(View.GONE);
+                            btn_regist.setVisibility(View.VISIBLE);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RegisterActivity.this,"ON Error Response!  " + error.toString(),Toast.LENGTH_SHORT).show();
+                Log.v("leo","ERROR Response : "+error.toString());
+                loading.setVisibility(View.GONE);
+                btn_regist.setVisibility(View.VISIBLE);
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                //前面的name是資料庫的name 後面的name是這裡的name = findViewbyid的name
+                params.put("name",name);
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+
+
+    }
+}
